e12fcc1a0b0d6568ab8074c48dfcbe6938756556
Lucas <lucas.p@ges.inatel.br>
2020-01-20 07:07:52 +0200

    first electa solar mvp

--- electa_solar/settings.py
+++ electa_solar/settings.py
@@ -125,3 +125,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/electasolar.pythonanywhere.com/static',
+]
